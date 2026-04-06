8f4ce56b66247cd1bfdaffdd2398a098db9d9cb5
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-06 10:39:33 +0000

    Adding the routes to the project

---
+++ admin-dashboard/src/routes/AppRoutes.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import Dashboard from "../pages/Dashboard";
+import Orders from "../pages/Orders";
+import Users from "../pages/Users";
+
+const AppRoutes = () => {
+    
+}
