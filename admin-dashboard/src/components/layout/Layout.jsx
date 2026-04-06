626653b2f2bd99a5c4a810e168d3ffeff1370c43
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-06 09:07:19 +0000

    Creating the folder structure of the project

---
+++ admin-dashboard/src/components/layout/Layout.jsx
@@ -0,0 +1,17 @@
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+const Layout = ({ children }) => {
+
+    return(
+        <div className="flex">
+            <Sidebar/>
+        <div className="flex-1">
+            <Navbar/>
+        <main className="p-4">{children}</main>
+        </div>
+        </div>
+    );
+};
+
+export default Layout;
