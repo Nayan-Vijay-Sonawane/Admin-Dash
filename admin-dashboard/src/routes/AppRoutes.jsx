72541974ce0efed80a3a42335a983216fc67f395
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-06 10:44:15 +0000

    Adding new routes to the project

--- admin-dashboard/src/routes/AppRoutes.jsx
+++ admin-dashboard/src/routes/AppRoutes.jsx
@@ -5,5 +5,18 @@ import Orders from "../pages/Orders";
 import Users from "../pages/Users";
 
 const AppRoutes = () => {
-    
-}
+    return(
+    <BrowserRouter>
+    <Layout>
+        <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" />} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+            <Route path="/users" element={<Users/>} />
+            <Route path="orders" element={<Orders/>} />
+        </Routes>
+    </Layout>
+    </BrowserRouter>
+    );
+};
+
+export default AppRoutes;
