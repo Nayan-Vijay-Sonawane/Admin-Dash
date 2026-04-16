7ab9126a5b6f2632f4083d0979801e1f649203bb
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-16 18:27:42 +0000

    Adding new users

--- admin-dashboard/src/pages/Users.jsx
+++ admin-dashboard/src/pages/Users.jsx
@@ -1,32 +1,30 @@
-import { useState, useEffect } from 'react';
-import getUsers from '../services/api';
+// src/pages/Users.jsx
+import { useState, useEffect } from "react";
+import getUsers from "../services/api";
 
 const Users = () => {
-    const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        getUsers
-            .then((res) => {
-                setUsers(res.data);
-                setLoading(false);
-            })
-            .catch(() => {
-                setLoading(false);
-            });
-    }, []);
-    
-    if (loading) return <p>Loading...</p>;
+  useEffect(() => {
+    getUsers()
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
 
-    return(
-        <div>
-            <h1>Users</h1>
-            {users.map((user) => (
-                <p key={users.id}>{users.name}</p>
-            ))}
-        </div>
-    );
+  if (loading) return <p>Loading...</p>;
 
+  return (
+    <div>
+      <h1>Users</h1>
+      {users.map((user) => (
+        <p key={user.id}>{user.name}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Users;
