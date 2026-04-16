8cfa78586d27b93e5cb05f26fee15682efc90f94
Nayan Vijay Sonawane <99018949+Nayan-Vijay-Sonawane@users.noreply.github.com>
2026-04-16 18:30:25 +0000

    Addding Dashboard

--- admin-dashboard/src/pages/Dashboard.jsx
+++ admin-dashboard/src/pages/Dashboard.jsx
@@ -1,9 +1,43 @@
+// src/pages/Dashboard.jsx
+import React, { useState, useEffect } from "react";
+import Card from "../components/ui/Card";
+import DataTable from "../components/tables/DataTable";
+import getUsers from "../services/api";
+
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getUsers()
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch(() => setLoading(false));
+  }, []);
+
+  const columns = [
+    { header: "Name", accessor: "name" },
+    { header: "Email", accessor: "email" },
+    { header: "Username", accessor: "username" },
+  ];
+
+  if (loading) return <p>Loading Dashboard...</p>;
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div style={{ display: "flex" }}>
+        <Card title="Total Users" value={users.length} />
+        <Card title="Active Users" value={users.length - 2} />
+        <Card title="New Signups" value={5} />
+      </div>
+
+      <h2>Users Table</h2>
+      <DataTable columns={columns} data={users} />
+    </div>
+  );
 };
 
 export default Dashboard;
